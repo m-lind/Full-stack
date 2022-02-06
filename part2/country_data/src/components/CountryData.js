db27fd4a963c8ba9f6a6f3babc7f31d86bcb65db
m-lind <mari.j.lindstedt@gmail.com>
2022-02-06 14:39:23 +0200

    Added current weather data in the capital to the single country detail view

--- part2/country_data/src/components/CountryData.js
+++ part2/country_data/src/components/CountryData.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const CountryData = ({ country }) => {
 
+    const [weatherData, setWeatherData] = useState('')
     const languages = Object.values(country.languages)
+    
+    const api_key = process.env.REACT_APP_API_KEY
+    const city = country.capital
+
+    const hook = () => {
+        axios
+          .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${city}`)
+          .then(response => {
+            setWeatherData(response.data.current)
+          })
+    }
+    
+    useEffect(hook, [api_key, city])
     
     return (
         <div>
@@ -21,6 +36,18 @@ const CountryData = ({ country }) => {
             <p>       
                 <img width='120px' height='auto' src={country.flags.svg} alt="flag"></img>
             </p>
+            <h2>Weather in {city}</h2>
+            <div>
+                <strong>temperature: </strong>
+                {weatherData.temperature} Celsius
+            </div>
+            <div>
+                <img src={weatherData.weather_icons} alt="weather"></img>
+            </div>
+            <div>
+                <strong>wind: </strong>
+                {weatherData.wind_speed} kilometers/hour direction {weatherData.wind_dir}
+            </div>
         </div>
     )
 }
