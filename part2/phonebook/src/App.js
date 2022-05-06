d60e11a8b1b261df3bdb05e4bdad5e14f631f883
m-lind <mari.j.lindstedt@gmail.com>
2022-05-06 15:33:49 +0300

    Removed the reference to localhost

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -32,7 +32,7 @@ const App = () => {
 
   const hook = () => {
     axios
-      .get('http://localhost:3001/api/persons')
+      .get('/api/persons')
       .then(response => {
         setPersons(response.data)
       })
